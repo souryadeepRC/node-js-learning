23167bbb2a19d882f0a3b29c3e4b413774b98643
Coder Sourya <thecreativeworldforall@gmail.com>
2024-02-06 13:50:43 +0530

    Split files

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
 const http = require("http");
-const fs = require("fs");
+const { handler } = require("./routes");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  if (req?.url === "/apply") {
-    res.setHeader("Content-Type", "text/html");
-    res.write(
-      `<html>
-        <body>
-          <form action="/message" method="POST">
-            <input type="text" name="message" value="Hello!"/>
-            <button type="submit">Send</button>
-          </form>
-        </body>
-      </html>`
-    );
-    return res.end();
-  }
-  if (req?.url === "/message" && req?.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const message = Buffer.concat(body).toString();
-      const textMsg = message?.split("=")?.[1];
-      fs.writeFileSync("database.txt", textMsg);
-      res.statusCode = 302;
-
-      res.setHeader("Content-Type", "text/html");
-      res.write(`<html>
-    <body>
-      <span>Received Data: ${textMsg}</span>
-    </body>
-  </html>`);
-      res.end();
-    }); 
-  }
-});
+const server = http.createServer(handler);
 server.listen(5000);
---
+++ routes.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+const reqHandler = (req, res) => {
+  if (req?.url === "/apply") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      `<html>
+        <body>
+          <form action="/message" method="POST">
+            <input type="text" name="message" value="Hello!"/>
+            <button type="submit">Send</button>
+          </form>
+        </body>
+      </html>`
+    );
+    return res.end();
+  }
+  if (req?.url === "/message" && req?.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const message = Buffer.concat(body).toString();
+      const textMsg = message?.split("=")?.[1];
+      fs.writeFileSync("database.txt", textMsg);
+      res.statusCode = 302;
+
+      res.setHeader("Content-Type", "text/html");
+      res.write(`<html>
+    <body>
+      <span>Received Data: ${textMsg}</span>
+    </body>
+  </html>`);
+      res.end();
+    });
+  }
+};
+
+module.exports = {
+  handler: reqHandler,
+};
